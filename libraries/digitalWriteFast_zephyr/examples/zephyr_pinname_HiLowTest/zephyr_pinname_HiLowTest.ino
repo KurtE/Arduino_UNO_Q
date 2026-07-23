27ebc95ac2ab138e5faba31dc6a6b41fa1460b85
Kurt Eckhardt <kurte@rockisland.com>
2026-07-23 15:28:42 -0700

    Move sketches into digitalWriteFast_zephyr library

--- libraries/digitalWriteFast_zephyr/examples/zephyr_pinname_HiLowTest/zephyr_pinname_HiLowTest.ino
+++ libraries/digitalWriteFast_zephyr/examples/zephyr_pinname_HiLowTest/zephyr_pinname_HiLowTest.ino
@@ -1,4 +1,26 @@
+//=============================================================================
+// Zephyr Pin test using my digitalWriteFast library for Zephyr (STM32)
+// It uses access to the pins using Pin Names.
+//
+// This sketch is setup to set all pins (minus thouse exceluded) into either
+// INPUT_PULUP or INPUT_PULLDOWN and then scans all of those pins to see if
+// they change state and if so prints out the new state.
+//
+// This allow you when in PULL Up mode to use a jumper wire to ground and try
+// touching the pins in question.  of in Pull Down mode connect pins up to
+// 3.3v.  When the pins change state, the code prints out the logical 
+// Pin name for that pin(s) and if the is in the Arduino Pin table it
+// also prints out the Arduino pin number.  Example on Portenta H7,
+// I touched logical pin 5 and it printed out:
+// PC_6(5) val=0,
+// Some boards like Portenta H7 Arduino tables do not currently
+// contain all of pins on the high density connectors, which if
+// connected up to one of their Carrier boards, may print out 
+// like: PI_5 val=0,
+//
+//=============================================================================
 #include <digitalWriteFast_zephyr.h>
+//#define PRINT_DEBUG_PIN_ENUM
 
 const char *pin_names[] = {
   // clang-format off
@@ -31,7 +53,8 @@ const uint8_t count_pin_names = sizeof(pin_names) / sizeof(pin_names[1]);
 uint8_t pin_test_mode = 1;
 
 // This one is setup currently for Portenta H7
-uint8_t pinLast[count_pin_names] = {
+uint8_t pinLast[count_pin_names] = {0};
+#if 0
   // clang-format off
     0x00, 0xff, 0xff, 0xff, 0x00, 0xff, 0x00, 0xff, //PA_0
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff,  
@@ -56,13 +79,13 @@ uint8_t pinLast[count_pin_names] = {
     0xff, 0x00, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff  //PK_0
   // clang-format on
 };
+#endif
 
 #if defined(ARDUINO_UNO_Q)
-static constuint16_t pinname_exclude_list[11] = {
+static const uint16_t pinname_exclude_list[] = {
   // LEDS PH_10=15, LED_MATRIX=PF0-10, SPI RDR=PG_13, Analog switch=PA_2, BOOT0 PH_3
   0b0000000000000100, 0b0000000000000000, 0b0000000000000000, 0b0000000000000000,  // GPIOA-D
-  0b0000000000000000, 0b0000011111111111, 0b0010000000000000, 0b1111110000001000,  // GPIOE-PH
-  0b0000000000000000, 0b0000000000000000, 0b0000000000000000,                      // GPIOI-K
+  0b0000000000000000, 0b0000011111111111, 0b1111111111111111, 0b1111110000001000  // GPIOE-PH
 };
 
 #elif defined(ARDUINO_PORTENTA_H7_M7)
@@ -70,19 +93,31 @@ static const uint16_t pinname_exclude_list[11] = {
   // LEDS PH_10=15, LED_MATRIX=PF0-10, SPI RDR=PG_13, Analog switch=PA_2, BOOT0 PH_3
   0b1000000010101110, 0b0011110000100011, 0b0101111100110011, 0b1111111100000111, // GPIOA-D
   0b1111111111110011, 0b1111111111111111, 0b1011100101110111, 0b0000000000111111, // GPIOE-PH
-  0b0001100000000000, 0b1111000000111111, 0b11111101                              // GPIOI-K
+  0b0001100000000000, 0b1111000000111111, 0b1111111111111101                              // GPIOI-K
 };
+#define PROMPT_FOR_ARDUINO_PINS_ONLY
+
 #elif defined(ARDUINO_GIGA)
+static const uint16_t pinname_exclude_list[] = {
+  0b0000000000000000, 0b0000000000000000, 0b0010000000000000, 0b0000000000000000,  // GPIOA-D
+  0b0000000000001000, 0b0000000000000000, 0b0000000000000000, 0b0000000000000000,  // GPIOE-PH
+  0b0001000000000000, 0b0010000000000000, 0b1111111100000000                      // GPIOI-K
+};
+#elif defined(ARDUINO_NICLA_VISION)
+static const uint16_t pinname_exclude_list[] = {
+  0b0000000000000000, 0b0000000000000000, 0b0010000000000000, 0b0000000000000000,  // GPIOA-D
+  0b0000000000001000, 0b0000000000000000, 0b0000000000000000, 0b0000000000000000,  // GPIOE-PH
+  0b0001000000000000, 0b0010000000000000, 0b1111111100000000                      // GPIOI-K
+};
+#else
 static const uint16_t pinname_exclude_list[11] = {0};
+#define PROMPT_FOR_ARDUINO_PINS_ONLY
 #endif
 
-#ifndef NUM_DIGITAL_PINS
-#define NUM_DIGITAL_PINS (22u)
 //#if DT_PROP_LEN(DT_PATH(zephyr_user), digital_pin_gpios) > 0
 //uint16_t PINS_COUNT = DT_PROP_LEN(DT_PATH(zephyr_user), digital_pin_gpios);
 const uint8_t PINS_COUNT = sizeof(pinLast);
 //#endif
-#endif
 
 int index_pinLast_used = -1;
 
@@ -92,21 +127,34 @@ extern void allPinTest();
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial && millis() < 10000)
-    ;
+  while (!Serial && millis() < 5000) {}
 
+  Serial.println("\n*** Pin High/Low Test for Zephyr STM32 Boards **");
+  
   // default to only Arduino Pins.
-  //enable_only_arduino_pins();
+  #ifdef PROMPT_FOR_ARDUINO_PINS_ONLY
+  Serial.println("Enable only Arduino defined pines? (Y/N) (Defaults No in 10 seconds)");
+
+  uint32_t start_time = millis();
+  while (!Serial.available() && (millis() - start_time) < 10000) {}
+  int ch = Serial.read();
+  if (ch == 'y' || ch == 'Y') 
+  #endif
+  {
+    Serial.println("*** Only Arduino pins enabled ***");
+    enable_only_arduino_pins();
+  }
+
   exclude_pins();
   // find the last pin name used
-  for (index_pinLast_used = sizeof(pinLast) - 1; index_pinLast_used >= 0; index_pinLast_used--) {
+  for (index_pinLast_used = (sizeof(pinname_exclude_list)/sizeof(pinname_exclude_list[0]))*16 - 1; index_pinLast_used >= 0; index_pinLast_used--) {
     if (pinLast[index_pinLast_used] == 0) break;
   }
 
   
 
-  Serial.print("NUM_DIGITAL_PINS: ");
-  Serial.println(NUM_DIGITAL_PINS, DEC);
+  Serial.print("NUM_OF_DIGITAL_PINS: ");
+  Serial.println(NUM_OF_DIGITAL_PINS, DEC);
   Serial.println("Pins included in test:");
   int index_first_in_series = -1;
   for (int i = 0; i <= index_pinLast_used; i++) {
@@ -146,7 +194,7 @@ void enable_only_arduino_pins() {
 
 void exclude_pins() {
   int pin_name_index = 0;
-  for (uint8_t i = 0; i < 11; i++) {
+  for (uint8_t i = 0; i < (sizeof(pinname_exclude_list)/sizeof(pinname_exclude_list[0])); i++) {
     uint16_t exclude_pins = pinname_exclude_list[i];
     for (uint8_t j = 0; j < 16; j++) {
       if(exclude_pins & 1) pinLast[pin_name_index] = 0xff;
@@ -172,8 +220,14 @@ void allPinTest() {
         Serial.flush();
       }
       pinMode(pin_name, INPUT_PULLUP);
+      #ifdef PRINT_DEBUG_PIN_ENUM
+      Serial.print(ii, HEX); Serial.flush();
+      #endif
       delayMicroseconds(5);
       pinLast[ii] = digitalReadFast(pin_name);
+      #ifdef PRINT_DEBUG_PIN_ENUM
+      Serial.print(":"); Serial.println(pinLast[ii]); Serial.flush();
+      #endif      
       if (!pinLast[ii]) {
         Serial.print("\nd#=");
         Serial.print(pin_names[ii]);
@@ -329,5 +383,9 @@ void show_all_gpio_regs() {
   print_gpio_regs("G", (GPIO_TypeDef *)GPIOG_BASE);
   print_gpio_regs("H", (GPIO_TypeDef *)GPIOH_BASE);
   print_gpio_regs("I", (GPIO_TypeDef *)GPIOI_BASE);
+  #if ! defined(ARDUINO_UNO_Q)
+  print_gpio_regs("J", (GPIO_TypeDef *)GPIOJ_BASE);
+  print_gpio_regs("K", (GPIO_TypeDef *)GPIOK_BASE);
+  #endif
 }
 
